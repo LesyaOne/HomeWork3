ce6d17c84961c8ce925c9cd7e675d940f96bc906
lesya_popova <lspopova99@gmail.com>
2023-06-20 10:37:57 +0800

    added task3

---
+++ taskThree/Program.cs
@@ -0,0 +1,27 @@
+﻿void CubePrinter(int number)
+{
+    if (number >=1)
+    {
+        int current = 1;
+        while (current <= number)
+        {
+            double cube = Math.Pow(current, 3);
+            Console.Write($"{cube}, ");
+            current++;
+        }
+    }
+    if (number < 1)
+    {
+        int current = 1;
+        while (current >= number)
+        {
+            double cube = Math.Pow(current, 3);
+            Console.Write($"{cube}, ");
+            current--;
+        }
+    }
+    Console.WriteLine("\b\b.");
+}
+Console.Write("Enter number: ");
+int num = Convert.ToInt32(Console.ReadLine());
+CubePrinter(num);
